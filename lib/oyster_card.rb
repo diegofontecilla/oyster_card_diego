cf5a9bd524f7dc3445e37234ff7fac2c9a46d659
anguspettifer <angus.pettifer@gmail.com>
2018-05-21 17:04:48 +0100

    max limit

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,11 +1,16 @@
 class Oystercard
   attr_reader :balance
 
-  def initialize
+  DEFAULT_MAX_LIMIT = 90
+
+  def initialize(max_amount = DEFAULT_MAX_LIMIT)
+    @max_amount = max_amount
     @balance = 0
   end
 
   def top_up(amount)
+    message = "Card top up failed: exceeded maximum balance"
+    fail message if (@balance + amount) > @max_amount
     @balance += amount
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,9 +7,16 @@ describe Oystercard do
   end
 
   describe '#top_up' do
+
     it 'tops up card' do
       subject.top_up(10)
       expect(subject.balance).to eq 10
     end
+
+    it 'limits top up' do
+      message = "Card top up failed: exceeded maximum balance"
+      expect { subject.top_up(95) } .to raise_error message
+    end
+
   end
 end
