b69695d67c99b2a860ab685328d325cc116471a0
diego fontecilla <mail@diegofontecilla.com>
2018-05-22 12:52:40 +0100

    end chapter 7

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,4 +1,4 @@
-class Oystercard
+class OysterCard
   attr_reader :balance
 
   DEFAULT_MAX_LIMIT = 90
@@ -11,5 +11,9 @@ class Oystercard
     message = "Failed: exceeded max balance #{DEFAULT_MAX_LIMIT}"
     fail message if (@balance + amount) > DEFAULT_MAX_LIMIT
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,6 +1,8 @@
 require 'oyster_card'
 
-describe Oystercard do
+describe OysterCard do
+
+  # it { is_expected.to respond_to(:deduct).with(1).argument }
 
   it "has a balance of zero" do
     expect(subject.balance).to eq 0
@@ -14,11 +16,17 @@ describe Oystercard do
     end
 
     it 'limits top up' do
-      max = Oystercard::DEFAULT_MAX_LIMIT
+      max = OysterCard::DEFAULT_MAX_LIMIT
       subject.top_up(max)
       message = "Failed: exceeded max balance #{max}"
       expect { subject.top_up(1) } .to raise_error message
     end
 
+    describe '#deduct' do
+      it 'amount on card' do
+        subject.top_up(30)
+        expect{ subject.deduct 3 }.to change{ subject.balance }.by -3
+      end
+    end
   end
 end
